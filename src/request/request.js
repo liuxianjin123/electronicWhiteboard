5186cbb52b31cfc18ca318d9c54ad24f629650d6
刘先近666 <517584080@qq.com>
2021-09-17 17:22:11 +0800

    123

--- src/request/request.js
+++ src/request/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-///接口文档地址：https://www.showdoccc/AIMS?page_id=4264603188555394
+///接口文档地址：https://www.showdoccc/AIMS?page_id=4264603188555394lxj
 const instance = axios.create({
         //baseURL: 'http://192.168.0.80:666', //正式库
         baseURL: 'http://192.168.1.9:5005', //白板
